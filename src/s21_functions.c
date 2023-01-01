96178a99c53cb3bf2b74235c6a7951b7091a6b0f
1 <1>
2023-01-01 19:16:04 +0300

    kukareku

--- src/s21_functions.c
+++ src/s21_functions.c
@@ -1,18 +1,20 @@
 #include "s21_string.h"
 
 int s21_strtok(char *string, char *delim) {
-    int i = 0;
-    char ch_buf = 0;
-    while(ch_buf != '\0') {
-        ch_buf = string[i];
-        i++;
-    }
-    printf("%d", ch_buf);
-	return 0;
+  int i = 0;
+  char ch_buf = 0;
+  while (ch_buf != '\0') {
+    ch_buf = string[i];
+    i++;
+  }
+  printf("%d", ch_buf);
+  return 0;
 }
 
 s21_size_t s21_strlen(char *string) {
-	s21_size_t len = 0;
-	for(; *(string + len); len++);
-	return len;
+  s21_size_t len = 0;
+  for (; *(string + len); len++)
+    ;
+  return len;
 }
+// КУ-КУ
