02f72727fbca411d0d795f951f6c61f6aa7c98c3
Im Vlad, and you? <vladrazd02@yandex.ru>
2023-01-01 20:26:32 +0300

    lol2000

--- src/s21_functions.h
+++ src/s21_functions.h
@@ -11,3 +11,7 @@ char s21_strcat(char *str_change, char *str_add);
 void s21_strncat(char *restrict s1, char *restrict s2, s21_size_t n);
 char s21_strerror(int errnum); 
 void s21_strcmp(char *str_one, char *str_two);
+void *s21_memchr(void const *str, int c, size_t n);
+char *s21_strchr(const char *str, int c);
+char *s21_strpbrk(const char *str1, const char *str2);
+char *s21_strrchr(const char *str, int c);
