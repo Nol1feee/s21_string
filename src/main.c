0563a11533fbc5a00af039fc396aecf209513ec7
Im Vlad, and you? <vladrazd02@yandex.ru>
2023-01-01 20:29:52 +0300

    one_more

--- src/main.c
+++
@@ -1,8 +0,0 @@
-#include "s21_functions.h"
-
-int main() {
-	char string[] = "Address family not supported by protocol family";
-	char lol[] = "c";
-	printf("%llu", s21_strlen(string));
-	return 0;
-}
