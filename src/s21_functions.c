5b31b87649a491e6d3635938400b6674b4436076
Im Vlad, and you? <vladrazd02@yandex.ru>
2023-01-03 17:02:10 +0300

    strtok && strerror 50/50

--- src/s21_functions.c
+++ src/s21_functions.c
@@ -1,55 +1,53 @@
 #include "s21_functions.h"
 
-//work great
+// work great
 s21_size_t s21_strlen(char *string) {
-	s21_size_t len = 0;
-	while(*(string + len)) {
-		len++;
-	}
-	return len;
+  s21_size_t len = 0;
+  while (*(string + len)) {
+    len++;
+  }
+  return len;
 }
 
-//don't work with NULL, otherwise work great
+// don't work with NULL, otherwise work great
 char s21_strtok(char *string, char *delim) {
-	s21_size_t len = s21_strlen(string);
-	for(int i = 0; i < len; i++) {
-		if(string[i] == *delim) 
-			string[i] = '\0';
-			break;
-	}
-	return string[i];
+  s21_size_t len = s21_strlen(string);
+  for (int i = 0; i < len; i++) {
+    if (string[i] == *delim) string[i] = '\0';
+    break;
+  }
+  return string[i];
 }
 
-//work great
-char *s21_strcat(char *str_change, char *str_add); {
-	return(s21_strncat(str_change, str_add, s21_strlen(str_change) + s21_strlen(str_add)));
+// work great
+char *s21_strcat(char *str_change, char *str_add);
+{
+  return (s21_strncat(str_change, str_add,
+                      s21_strlen(str_change) + s21_strlen(str_add)));
 }
 
-//work great
+// work great
 char *s21_strncat(char *str_change, char *str_add, s21_size_t n) {
-	size_t len_str_change = strlen(str_change);
-	int i = 0;
-	while(str_add[i] != '\0' && i < n) {
-		str_change[len_str_change + i] = str_add[i]; 
-		i++;
-	}
-	str_change[len_str_change + i] = '\0';
-	return str_change;
+  size_t len_str_change = strlen(str_change);
+  int i = 0;
+  while (str_add[i] != '\0' && i < n) {
+    str_change[len_str_change + i] = str_add[i];
+    i++;
+  }
+  str_change[len_str_change + i] = '\0';
+  return str_change;
 }
 
-char *s21_strerror(int errnum) { 
-	// ошибка без static - address of stack memory associated with local variable 'res' returned [-Wreturn-stack-address]
-	static char res[50] = {0};
-	if(errnum >= 0 && errnum < AMOUNT_ERRORS) {
-		strcpy(res, errors[errnum]);
-	}// else {
-	//	printf("Unknown error: %d", errnum);
-	//}
-	return res;
-}
-
-<<<<<<< HEAD
-void s21_strcmp(char *str_one, char *str_two) {
+char *s21_strerror(int errnum) {
+  // ошибка без static - address of stack memory associated with local variable
+  // 'res' returned [-Wreturn-stack-address]
+  static char res[50] = {0};
+  if (errnum >= 0 && errnum < AMOUNT_ERRORS) {
+    strcpy(res, errors[errnum]);
+  }  // else {
+  //	printf("Unknown error: %d", errnum);
+  //}
+  return res;
 }
 
 void *s21_memchr(void const *str, int c, size_t n) {
@@ -88,7 +86,6 @@ char *s21_strpbrk(const char *str1, const char *str2) {
       }
     }
   }
-
   return flag == 1 ? (char *)&str1[pos] : NULL;
 }
 // Выполняет поиск последнего вхождения символа c (беззнаковый тип) в строке, на
@@ -101,12 +98,12 @@ char *s21_strrchr(const char *str, int c) {
   } while (*str++);
   return (res);
 =======
-//work great
-int s21_strcmp(char *str_one, char *str_two) {
-	while(*str_one && *str_one == *str_two) {
-		str_one++;
-		str_two++;
-	}
-return *str_one - *str_two;
+  // work great
+  int s21_strcmp(char *str_one, char *str_two) {
+    while (*str_one && *str_one == *str_two) {
+      str_one++;
+      str_two++;
+    }
+    return *str_one - *str_two;
 >>>>>>> vlad
-}
+  }
