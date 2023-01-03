39eb50c0eacbfd3b7e339f322a29dbe2bfab2336
Snape Burl <snapebur@mi-f4.msk.21-school.ru>
2023-01-03 22:18:58 +0300

    lel

--- src/s21_functions.h
+++ src/s21_functions.h
@@ -13,4 +13,3 @@ char *s21_strtok(char *string, char *delim);
 char *s21_strcat(char *str_change, char *str_add);
 char *s21_strncat(char *str_change, char *str_add, s21_size_t n) ;
 char *s21_strerror(int errnum); 
-int s21_strcmp(char *str_one, char *str_two);
