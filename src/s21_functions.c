f5d9c33cf623bbc97a1601286ebcac2ea6333c28
Snape Burl <snapebur@mi-f4.msk.21-school.ru>
2023-01-03 22:17:55 +0300

    fix strtok

--- src/s21_functions.c
+++ src/s21_functions.c
@@ -1,6 +1,6 @@
 #include "s21_functions.h"
 
-//work great
+//возвращает длину строки без '\0'
 s21_size_t s21_strlen(char *string) {
 	s21_size_t len = 0;
 	while(*(string + len)) {
@@ -10,22 +10,22 @@ s21_size_t s21_strlen(char *string) {
 }
 
 //don't work with NULL, otherwise work great
-char s21_strtok(char *string, char *delim) {
+char *s21_strtok(char *string, char *delim) {
 	s21_size_t len = s21_strlen(string);
 	for(int i = 0; i < len; i++) {
 		if(string[i] == *delim) 
 			string[i] = '\0';
 			break;
 	}
-	return string[i];
+	return string;
 }
 
-//work great
-char *s21_strcat(char *str_change, char *str_add); {
+//фун-ия добавляет в конец str_change содержание str_add
+char *s21_strcat(char *str_change, char *str_add) {
 	return(s21_strncat(str_change, str_add, s21_strlen(str_change) + s21_strlen(str_add)));
 }
 
-//work great
+//фун-ия добавляет в конец str_change содержание str_add не более n байт 
 char *s21_strncat(char *str_change, char *str_add, s21_size_t n) {
 	size_t len_str_change = strlen(str_change);
 	int i = 0;
@@ -36,19 +36,21 @@ char *s21_strncat(char *str_change, char *str_add, s21_size_t n) {
 	str_change[len_str_change + i] = '\0';
 	return str_change;
 }
-
+//выводит сообщение об ошибке (для линукса и мак ос разные)
+//заменить спринтф на с21_спринтф!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 char *s21_strerror(int errnum) { 
-	// ошибка без static - address of stack memory associated with local variable 'res' returned [-Wreturn-stack-address]
 	static char res[50] = {0};
 	if(errnum >= 0 && errnum < AMOUNT_ERRORS) {
 		strcpy(res, errors[errnum]);
-	}// else {
-	//	printf("Unknown error: %d", errnum);
-	//}
+	} else {
+		sprintf(res, "Unknown error: %d", errnum);
+	}
 	return res;
 }
 
-//work great
+//фун-ия посимвольно сравнивает 2 строки, если они равны - возвращает 0,
+//если 1ый несовпадающий символ 1ой строки "больше" - возвращает положительное число,
+//если символ второй строки больше - отрицательное
 int s21_strcmp(char *str_one, char *str_two) {
 	while(*str_one && *str_one == *str_two) {
 		str_one++;
--- src/s21_functions.h
+++ src/s21_functions.h
@@ -1,12 +1,15 @@
+#pragma once 
+
 #define s21_size_t unsigned long long 
 #define s21_NULL (void*)0
 
 #include <stdio.h>
 #include <string.h>
+
 #include "s21_errors.h"
 
 s21_size_t s21_strlen(char *string);
-char s21_strtok(char *string, char *delim); 
+char *s21_strtok(char *string, char *delim); 
 char *s21_strcat(char *str_change, char *str_add);
 char *s21_strncat(char *str_change, char *str_add, s21_size_t n) ;
 char *s21_strerror(int errnum); 
