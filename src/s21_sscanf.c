b8eaa6656ed9363d93078750a9a2281abc82f8bb
katharin <katharin@student.21-school.ru>
2023-01-29 22:35:49 +0400

    katharin: first commit of sscanf

---
+++ src/s21_sscanf.c
@@ -0,0 +1,10 @@
+/*sscanf implementation*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+int s21_sscanf(const char *str, const char *format, ...) {
+  /* function body */
+  return 0;
+}
