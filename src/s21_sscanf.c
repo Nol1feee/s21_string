575ca875ab6e0d57e3161a256b7a85c002dbf11f
katharin <katharin@student.21-school.ru>
2023-01-30 21:36:32 +0400

    katharin: sscanf works for simple string

--- src/s21_sscanf.c
+++ src/s21_sscanf.c
@@ -3,11 +3,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
-#include <string.h> /* for strlen (then delete)*/
+#include <string.h> /* for strlen, strcpy (then delete)*/
 
 int s21_sscanf(const char *str, const char *format, ...) {
   if (format) { // temporary!! to avoid the warning
   char *buffer = (char *)malloc(strlen(str) * sizeof(char));
+  strcpy(buffer, str);
   puts(buffer);
   }
   return 0;
--- src/sscanf_driver.c
+++ src/sscanf_driver.c
@@ -2,12 +2,14 @@
 
 #include <stdio.h>
 
+int s21_sscanf(const char *str, const char *format, ...);
+
 int main() {
   const char *src = "test";
   char dst[20];
-  //char test_dst[20];
+  char test_dst[20];
   sscanf(src, "%s", dst); 
-  //s21_sscanf(test_str, "%s", test_dst); 
+  s21_sscanf(src, "%s", test_dst); 
   puts(dst);
   return 0;
 }
