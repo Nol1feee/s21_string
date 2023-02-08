a7a83f6c88b8d73901ede826eaf1dfb2050963e7
SemenutaEgor <e.semenuta@yanex.ru>
2023-02-08 11:53:44 +0400

    katharin_t: added width to %f

--- src/sscanf_driver.c
+++ src/sscanf_driver.c
@@ -5,7 +5,7 @@
 int s21_sscanf(const char *str, const char *format, ...);
 
 int main() {
-  const char *src = "10.5";
+  const char *src = "111.111";
   //char dst[20];
   //char dst2[20];
   //char test_dst[20];
@@ -13,10 +13,10 @@ int main() {
   float num = 0.0;
   float test_num = 0.0;
   puts("===sscanf===");
-  sscanf(src, "%f", &num); 
+  sscanf(src, "%6f", &num); 
   printf("num: %f\n", num);
   puts("===s21_sscanf===");
-  s21_sscanf(src, "%f", &test_num); 
+  s21_sscanf(src, "%6f", &test_num); 
   printf("test_num: %f\n", test_num);
   return 0;
 }
