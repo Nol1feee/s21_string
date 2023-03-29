f858898c08dc5410f3ea8541fedf06c8f1f2b480
katharin <katharin@student.21-school.ru>
2023-03-29 21:44:14 +0400

    katharin: fixed LONG_MAX

--- src/sprintf_driver.c
+++ src/sprintf_driver.c
@@ -8,15 +8,19 @@ int main(void) {
   int res = 0, test_res = 0;
   char str1[100];
   char str2[100];
-  char *str3 = "Hello %d %023d";
-  int val = -75;
-
+  char *str3 = "%ld Test %ld Test %hd GOD %hd tt %d tt %d";
+  long int val = LONG_MAX;
+  long val2 = LONG_MIN;
+  short int val3 = SHRT_MAX;
+  short val4 = SHRT_MIN;
+  int val5 = INT_MAX;
+  int val6 = INT_MIN;
   printf("\n===sprintf===\n");
-  res = sprintf(str1, str3, val, val);
+  res = sprintf(str1, str3, val, val2, val3, val4, val5, val6);
   printf("\nstr1: %s\n", str1);
   printf("res: %d\n", res);
   printf("\n===s21_sprintf===\n");
-  test_res = s21_sprintf(str2, str3, val, val);
+  test_res = s21_sprintf(str2, str3, val, val2, val3, val4, val5, val6);
   printf("\nstr2: %s\n", str2);
   printf("test_res: %d\n", test_res);
   return 0;
